1eaa7f5ad8245ce1dfc786f9280519c34857def9
mohameddahroug <mhmeddahroug@gmail.com>
2025-07-24 22:48:58 +0300

    edit colors

--- src/screens/createProduct/CreateProduct.js
+++ src/screens/createProduct/CreateProduct.js
@@ -12,17 +12,17 @@ import {
   TouchableWithoutFeedback,
   ActivityIndicator,
   FlatList,
+  StatusBar,
 } from "react-native";
 import { launchImageLibrary } from "react-native-image-picker";
 import * as ImagePicker from "expo-image-picker";
 import { Picker } from "@react-native-picker/picker";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API } from "@env";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const CreateProduct = ({navigation}) => {
-
-//   const categoryId = "cmdc3qxl8000a12kc49thcm72";
-//   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNtZGRmNTVxNzAwMDBzNnlweG5oaThtOGgiLCJyb2xlIjoic2VsbGVyIiwiaWF0IjoxNzUzMTIxMjg1LCJleHAiOjE3NTM3MjYwODV9.EjqeiVhVpkBWo3kyJDO5ngPOHzWUAx3_kbis8kxoBxY";
+const CreateProduct = ({ navigation }) => {
   const [productName, setProductName] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
@@ -33,7 +33,6 @@ const CreateProduct = ({navigation}) => {
   const [status, setStatus] = useState("");
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
-  
 
   const pickImages = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -49,10 +48,6 @@ const CreateProduct = ({navigation}) => {
 
   //   send data to api
   const handleSave = async () => {
-    //   if (!productName || !price || !description || !category ||!stock || images.length === 0) {
-    //     Alert.alert('Error', 'Please fill in all the required fields and select at least one image.');
-    //     return;
-    //   }
     if (!productName) {
       Alert.alert("Error", "Please enter product name");
       return;
@@ -73,15 +68,14 @@ const CreateProduct = ({navigation}) => {
       return;
     }
 
-
     try {
-        const token = await AsyncStorage.getItem('token');
-        setLoading(true);
+      const token = await AsyncStorage.getItem("token");
+      setLoading(true);
       const formData = new FormData();
       formData.append("title", productName);
       formData.append("price", price);
       formData.append("description", description);
-      formData.append("discount", discount?discount:0);
+      formData.append("discount", discount ? discount : 0);
       formData.append("category_id", category);
       formData.append("stock", stock);
       formData.append("status", status);
@@ -93,25 +87,21 @@ const CreateProduct = ({navigation}) => {
           name: `image_${index}.jpg`,
         });
       });
-      const response = await fetch(
-        `${API}/products`,
-        {
-          method: "POST",
-          headers: {
-            // 'Content-Type': 'multipart/form-data',
-            Authorization: `Bearer ${token}`,
-          },
-          body: formData,
-        }
-      );
+      const response = await fetch(`${API}/products`, {
+        method: "POST",
+        headers: {
+          // 'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
       const data = await response.json();
       if (response.ok) {
         Alert.alert("Success", "Product added successfully", [
           {
             text: "OK",
             onPress: () => {
-              // navigation.navigate('Home');
-            //   console.log("OK");
+              //   navigation.goBack();
               setProductName("");
               setPrice("");
               setDescription("");
@@ -125,30 +115,26 @@ const CreateProduct = ({navigation}) => {
         ]);
       } else {
         Alert.alert("Error", data.error || "Failed to add product");
-        console.log(response)
+        console.log(response);
       }
-    //   console.log(data);
+      //   console.log(data);
     } catch (error) {
       console.error(error);
       Alert.alert("Error", "Failed to add product");
-    }finally{
-        setLoading(false);
+    } finally {
+      setLoading(false);
     }
-
   };
   //   get categories
   useEffect(() => {
     const fetchCategories = async () => {
-        const token = await AsyncStorage.getItem('token');
+      const token = await AsyncStorage.getItem("token");
       try {
-        const response = await fetch(
-          `${API}/categories/seller`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        const response = await fetch(`${API}/categories/seller`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         const data = await response.json();
         // console.log("Categories:", data);
         setCategories(data);
@@ -161,172 +147,175 @@ const CreateProduct = ({navigation}) => {
   }, []);
 
   const removeImage = (indexToRemove) => {
-  const updatedImages = images.filter((_, index) => index !== indexToRemove);
-  setImages(updatedImages);
-};
+    const updatedImages = images.filter((_, index) => index !== indexToRemove);
+    setImages(updatedImages);
+  };
 
   return (
-    <KeyboardAvoidingView
-    //   style={{ flex: 1 }}
-      behavior={"padding"}
-      keyboardVerticalOffset={20}
-    >
-        
-    {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss}> */}
-    <ScrollView contentContainerStyle={styles.container}
-    keyboardShouldPersistTaps="handled"
-    
-    >
-      <Text style={styles.title}>Add Product</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView behavior={"padding"} keyboardVerticalOffset={20}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
 
-      {categories?.length>0? null:<View style={styles.loadingContainer}>
-        <Text style={styles.title}>not categories yet plesse add category</Text>
-        <TouchableOpacity style={styles.saveBtn} onPress={() => navigation.navigate("CreateCategory")}>
-        <Text style={styles.saveBtnText}>Add Category</Text>
-      </TouchableOpacity>   
-      </View>}
-        
-        
-    
-      <TouchableOpacity style={styles.imageUploadBox} onPress={pickImages}>
-        <Text style={styles.title}>Add a product image</Text>
-        <Text style={styles.imageUploadText}>Upload a photo of your product</Text>
-        <Text style={styles.imageUploadBtn}>Upload image</Text>
-      </TouchableOpacity>
-
-      {/* <View style={styles.imagePreviewContainer}>
-        {images.map((img, i) => (
-          <Image key={i} source={{ uri: img.uri }} style={styles.image} />
-        ))}
-      </View> */}
-      <FlatList
-  horizontal
-  data={images}
-  keyExtractor={(item, index) => index.toString()}
-  renderItem={({ item, index }) => (
-    <View style={{ marginRight: 10, position: 'relative' }}>
-      <Image
-        source={{ uri: item.uri }}
-        style={{ width: 100, height: 100, borderRadius: 10 }}
-      />
-      <TouchableOpacity
-        onPress={() => removeImage(index)}
-        style={{
-          position: 'absolute',
-          top: -5,
-          right: -5,
-          backgroundColor: 'red',
-          borderRadius: 10,
-          padding: 5,
-        }}
-      >
-        <Text style={{ color: 'white', fontWeight: 'bold' }}>X</Text>
-      </TouchableOpacity>
-    </View>
-  )}
-/>
-
-      {/* <TouchableWithoutFeedback }> */}
-      <Text style={styles.label}>Product Name</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter product name"
-        placeholderTextColor={"#4F479E"}
-        value={productName}
-        onChangeText={setProductName}
-      />
-      <Text style={styles.label}>Price</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter price"
-        placeholderTextColor={"#4F479E"}
-        keyboardType="numeric"
-        value={price}
-        onChangeText={setPrice}
-      />
-      <Text style={styles.label}>Discount</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter discount (optional)"
-        placeholderTextColor={"#4F479E"}
-        keyboardType="numeric"
-        value={discount}
-        onChangeText={setDiscount}
-      />
-      {/* <TextInput
-        style={styles.input}
-        placeholder="Enter category"
-        value={category}
-        onChangeText={setCategory}
-      /> */}
-      <Text style={styles.label}>Category</Text>
-      <View style={styles.pickerWrapper}>
-        <Picker
-          selectedValue={category}
-          onValueChange={(itemValue) => setCategory(itemValue)}
-          style={styles.picker}
+        <ScrollView
+          contentContainerStyle={styles.container}
+          keyboardShouldPersistTaps="handled"
         >
-          <Picker.Item label="Select Category" value=""  color="#4F479E" enabled={false} />
-          {categories?.map((category) => (
-            <Picker.Item
-              key={category.id}
-              label={category.name}
-              value={category.id}
-            />
-          ))}
-        </Picker>
-      </View>
-      <Text style={styles.label}>Stock</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter stock"
-        keyboardType="numeric"
-        placeholderTextColor={"#4F479E"}
-        value={stock}
-        onChangeText={setStock}
-      />
-      {/* <TextInput
-        style={styles.input}
-        placeholder="Enter status"
-        value={status}
-        onChangeText={setStatus}
-      /> */}
-      <Text style={styles.label}>Status</Text>
-      <View style={styles.pickerWrapper}>
-        <Picker
-          selectedValue={status}
-          onValueChange={(itemValue) => setStatus(itemValue)}
-          style={styles.picker}
-        >
-          <Picker.Item label="Select status" value="" color="#4F479E" enabled={false} />
-          <Picker.Item label="Active" value="active" />
-          <Picker.Item label="Inactive" value="inactive" />
-        </Picker>
-      </View>
-      <Text style={styles.label}>Description</Text>
-      <TextInput
-        style={[styles.input, { height: 120 }]}
-        placeholder="description"
-        placeholderTextColor={"#4F479E"}
-        multiline
-        textAlignVertical="top"
-        numberOfLines={5}
-        value={description}
-        onChangeText={setDescription}
-      />
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.backButton}
+          >
+            <AntDesign name="arrowleft" size={24} color="black" />
+          </TouchableOpacity>
+          <Text style={styles.title}>Add Product</Text>
 
-      {loading ? (<View style={styles.saveBtn}>
+          {categories?.length > 0 ? null : (
+            <View style={styles.loadingContainer}>
+              <Text style={styles.title2}>
+                not categories yet plesse add category
+              </Text>
+              <TouchableOpacity
+                style={styles.saveBtn}
+                onPress={() => navigation.navigate("CreateCategory")}
+              >
+                <Text style={styles.saveBtnText}>Add Category</Text>
+              </TouchableOpacity>
+            </View>
+          )}
 
-        <ActivityIndicator size="large" color="#fff" />
-      </View>
-      ):
-        <TouchableOpacity style={styles.saveBtn} onPress={handleSave}>
-        <Text style={styles.saveBtnText}>Save</Text>
-      </TouchableOpacity>}
-      {/* </TouchableWithoutFeedback> */}
-    </ScrollView>
-    {/* </TouchableWithoutFeedback> */}
-    </KeyboardAvoidingView>
+          <TouchableOpacity style={styles.imageUploadBox} onPress={pickImages}>
+            <Text style={styles.title2}>Add a product image</Text>
+            <Text style={styles.imageUploadText}>
+              Upload a photo of your product
+            </Text>
+            <Text style={styles.imageUploadBtn}>Upload image</Text>
+          </TouchableOpacity>
+
+          <FlatList
+            horizontal
+            data={images}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <View style={{ marginRight: 10, position: "relative" }}>
+                <Image
+                  source={{ uri: item.uri }}
+                  style={{ width: 100, height: 100, borderRadius: 10 }}
+                />
+                <TouchableOpacity
+                  onPress={() => removeImage(index)}
+                  style={{
+                    position: "absolute",
+                    top: -5,
+                    right: -5,
+                    backgroundColor: "red",
+                    borderRadius: 10,
+                    padding: 5,
+                  }}
+                >
+                  <Text style={{ color: "white", fontWeight: "bold" }}>X</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+
+          <Text style={styles.label}>Product Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter product name"
+            placeholderTextColor={"grey"}
+            value={productName}
+            onChangeText={setProductName}
+          />
+          <Text style={styles.label}>Price</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter price"
+            placeholderTextColor={"grey"}
+            keyboardType="numeric"
+            value={price}
+            onChangeText={setPrice}
+          />
+          <Text style={styles.label}>Discount</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter discount (optional)"
+            placeholderTextColor={"grey"}
+            keyboardType="numeric"
+            value={discount}
+            onChangeText={setDiscount}
+          />
+
+          <Text style={styles.label}>Category</Text>
+          <View style={styles.pickerWrapper}>
+            <Picker
+              selectedValue={category}
+              onValueChange={(itemValue) => setCategory(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item
+                label="Select Category"
+                value=""
+                color="grey"
+                enabled={false}
+              />
+              {categories?.map((category) => (
+                <Picker.Item
+                  key={category.id}
+                  label={category.name}
+                  value={category.id}
+                />
+              ))}
+            </Picker>
+          </View>
+          <Text style={styles.label}>Stock</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter stock"
+            keyboardType="numeric"
+            placeholderTextColor={"grey"}
+            value={stock}
+            onChangeText={setStock}
+          />
+          <Text style={styles.label}>Status</Text>
+          <View style={styles.pickerWrapper}>
+            <Picker
+              selectedValue={status}
+              onValueChange={(itemValue) => setStatus(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item
+                label="Select status"
+                value=""
+                color="grey"
+                enabled={false}
+              />
+              <Picker.Item label="Active" value="active" />
+              <Picker.Item label="Inactive" value="inactive" />
+            </Picker>
+          </View>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            style={[styles.input, { height: 120 }]}
+            placeholder="description"
+            placeholderTextColor={"grey"}
+            multiline
+            textAlignVertical="top"
+            numberOfLines={5}
+            value={description}
+            onChangeText={setDescription}
+          />
+
+          {loading ? (
+            <View style={styles.saveBtn}>
+              <ActivityIndicator size="large" color="#fff" />
+            </View>
+          ) : (
+            <TouchableOpacity style={styles.saveBtn} onPress={handleSave}>
+              <Text style={styles.saveBtnText}>Save</Text>
+            </TouchableOpacity>
+          )}
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
@@ -334,20 +323,24 @@ export default CreateProduct;
 
 const styles = StyleSheet.create({
   container: {
-    // marginTop: 30,
-    // padding: 30,
-    // backgroundColor: "#fff",
-    // alignItems: "stretch",
-    // // flex: 1,
-   
     padding: 20,
     paddingBottom: 50,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 15,
+    marginBottom: 20,
+    marginTop: 10,
+    color: "#333",
+    textAlign: "center",
+  },
+  title2: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 10,
+    color: "#333",
     textAlign: "center",
   },
   imageUploadBox: {
@@ -360,8 +353,8 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   imageUploadText: {
-    color: "#666",
-    fontSize: 16,
+    color: "#665491",
+    fontSize: 14,
   },
   imageUploadBtn: {
     backgroundColor: "#E8E5F5",
@@ -391,8 +384,8 @@ const styles = StyleSheet.create({
   },
   input: {
     borderWidth: 1,
-    borderColor: "#ccc",
-    backgroundColor: "#E8E5F5",
+    borderColor: "#7569FA",
+    backgroundColor: "#fff",
     borderRadius: 10,
     paddingHorizontal: 15,
     paddingVertical: 10,
@@ -400,11 +393,11 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   saveBtn: {
-    backgroundColor: "#5E2BD9",
-    paddingVertical: 15,
-    borderRadius: 10,
+    backgroundColor: "#7569fa",
+    padding: 16,
+    borderRadius: 20,
     alignItems: "center",
-    marginTop: 5,
+    marginTop: 16,
   },
   saveBtnText: {
     color: "#fff",
@@ -413,13 +406,27 @@ const styles = StyleSheet.create({
   },
   pickerWrapper: {
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: "#7569FA",
     borderRadius: 6,
     marginBottom: 15,
-    backgroundColor: "#E8E5F5",
+    backgroundColor: "#fff",
   },
   picker: {
     height: 60,
     width: "100%",
+  },
+  loadingContainer: {
+    marginBottom: 20,
+  },
+  backButton: {
+    position: "absolute",
+    top: 30,
+    left: 20,
+    zIndex: 3,
+    padding: 8,
+    // backgroundColor: "#E8E5F5",
+    // borderRadius: 50,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
